76419a0c2dc57f5a03bfeeafebf2cfb98ba5d0f5
TeresaReLo <joannatere@yahoo.com.mx>
2024-07-19 11:35:56 -0600

    top properties

--- verif/formal/fv_funct_generator_multi_i.sv
+++ verif/formal/fv_funct_generator_multi_i.sv
@@ -1,12 +1,21 @@
 import gen_fifo_defines_pkg::*;
 
-module fv_funct_generator_multi_i (
+module fv_funct_generator_multi (
 	input  logic 				enh, 
 	input  logic signed[`DATA_WIDTH-1:0]	a_i,
 	input  logic signed[`DATA_WIDTH-1:0]	b_i,
 	input logic signed [(`DATA_WIDTH*2)-1:0] data_o
 );
 	`define CLK_PATH fv_generator_inst.clk
+	`define RST_PATH fv_generator_inst.rst
+	bit flag;
+
+  	always @(posedge `CLK_PATH) begin
+      	if (`RST_PATH == 1'b1)
+        	flag <= 1'b0;
+      	else 
+        	flag <=1'b1;
+  	end
 
 ///////////////////////////////////////////////////// Assumptions /////////////////////////////////////////////
 
@@ -18,8 +27,8 @@ module fv_funct_generator_multi_i (
       	multiplication_correct: assert property (@(posedge `CLK_PATH) (enh) |-> (data_o == (a_i * b_i))) $info("Assetion pas smultiplication_correct");
 	else $error(" Asserion fail multiplication_correct");
       	
-	// 2) The property assures that when enh is low the output data_o remains unchanged. //changing $past for $stable and |=> for |->
-      	data_o_stability_when_enh_0: assert property (@(posedge `CLK_PATH) (!enh) |-> ($stable(data_o))) $info("Assetion pass data_o_stability_when_enh_0");
+	// 2) The property assures that when enh is low the output data_o remains unchanged. //changing $past for $stable and |=> for |-> and adding flag
+      	data_o_stability_when_enh_0: assert property (@(posedge `CLK_PATH) ((!enh) && flag) |-> ($stable(data_o))) $info("Assetion pass data_o_stability_when_enh_0");
 	else $error(" Asserion fail data_o_stability_when_enh_0");
 
  
